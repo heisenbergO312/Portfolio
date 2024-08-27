0f7fcb84a78455c7d691cf43e3f39a34107d8df5
heisenbergO312 <heisenberg.03123@gmail.com>
2024-08-27 11:39:23 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JS, JSX, TS, and TSX files in the src directory
+    './public/index.html', // Include your public index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
